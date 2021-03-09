724dad99422d5d86f9b7207af594bd5a574c2b56
aryabhattabanerjee <aryabhattabanerjee8aroll9@gmail.com>
2021-03-09 16:01:37 +0530

    updated

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ function draw(){
 
 function mouseDragged(){
     //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY });
     //}
 }
 
@@ -93,8 +93,11 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32&& bird.body.speed<3){
+       bird.trajectory=[]
+       Matter.Body.setPosition(bird.body, {x: 200 , y: 50 });
        slingshot.attach(bird.body);
+       
     }
 }
 
